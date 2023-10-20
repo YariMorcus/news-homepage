b519c557d10620a8e794c7523a77e4d7e1f81791
YariMorcus <yarimorcus@zeelandnet.nl>
2023-10-20 19:37:12 +0200

    Add overlay

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,18 @@ document.querySelector('html').classList.add('js');
 const body = document.querySelector('body');
 const toggle = document.getElementById('toggle');
 const menu = document.getElementById('menu');
+const overlay = document.querySelector('.overlay');
 
 toggle.addEventListener('click', function () {
   if (menu.classList.contains('is-active')) {
     this.setAttribute('aria-expanded', 'false');
     menu.classList.remove('is-active');
     this.classList.remove('news-nav__hamburger-button--is-active');
-    body.classList.remove('disabled-scroll');
+    overlay.classList.remove('overlay--is-active');
   } else {
     menu.classList.add('is-active');
     this.classList.add('news-nav__hamburger-button--is-active');
-    body.classList.add('disabled-scroll');
     this.setAttribute('aria-expanded', 'true');
+    overlay.classList.add('overlay--is-active');
   }
 });
