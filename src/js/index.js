7a0f200532f4955e8f6f1a3c383e04fceed66429
YariMorcus <yarimorcus@zeelandnet.nl>
2023-10-20 14:40:15 +0200

    nav: disable scroll when menu is open

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 document.querySelector('html').classList.add('js');
 
+const body = document.querySelector('body');
 const toggle = document.getElementById('toggle');
 const menu = document.getElementById('menu');
 
@@ -7,8 +8,12 @@ toggle.addEventListener('click', function () {
   if (menu.classList.contains('is-active')) {
     this.setAttribute('aria-expanded', 'false');
     menu.classList.remove('is-active');
+    this.classList.remove('news-nav__hamburger-button--is-active');
+    body.classList.remove('disabled-scroll');
   } else {
     menu.classList.add('is-active');
+    this.classList.add('news-nav__hamburger-button--is-active');
+    body.classList.add('disabled-scroll');
     this.setAttribute('aria-expanded', 'true');
   }
 });
